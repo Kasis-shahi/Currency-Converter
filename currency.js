17c448d9306e248f8ddbed12bb1943bfc832db10
Kasis Shahi <kasisshhahi@gmail.com>
2026-08-19 16:58:23 +0545

    done

--- currency.js
+++ currency.js
@@ -1,6 +1,11 @@
 const fromDropDown=document.querySelector("#from-currency-select");
 const toDropDown=document.querySelector("#to-currency-select");
 let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+let result=document.querySelector("#result");
+
+const initialValue=document.querySelector("#amount")
+
+
 
 //todo creating drop-down options of  from currency-js file
 
@@ -15,7 +20,7 @@ currencies.forEach((currency)=>{
     
 });
 
-//creating drop-down options of  to currency-js file
+//todo creating drop-down options of  to currency-js file
 currencies.forEach((currency)=>{
     const option= document.createElement("option");
     option.value=currency;
@@ -31,11 +36,25 @@ currencies.forEach((currency)=>{
 
 let currencyConvert= async()=>{
 const amount=document.querySelector("#amount").value;
+initialValue.value="";
+
+//todo special features as i click in drop down, result seems vacant, only when i clic in convert, it shows;
+ toDropDown.addEventListener("change",()=>{
+    result.textContent="";
+ });
+ fromDropDown.addEventListener("change",()=>{
+    result.textContent="";
+ });
 
 const fromCurrency=fromDropDown.value;
+
 const toCurrency=toDropDown.value;
 
-//if input is empty
+
+
+
+
+//todo if input is empty
 
 if(amount===""){
     alert("Please fill the amount");
@@ -52,14 +71,15 @@ let toExchange= data.conversion_rates[toCurrency];
 //todo the actual answer
 const answerAmount= (amount/fromExchange)*toExchange;
 const roundAnswer=answerAmount.toFixed(3);
-let result=document.querySelector("#result");
+
 result.textContent=`${amount} ${fromCurrency}= ${roundAnswer} ${toCurrency}`;
 
 }
-
-
 }
 
 
+
+
 document.querySelector("#convert-button").addEventListener("click",currencyConvert);
- window.addEventListener("load",currencyConvert)
+ window.addEventListener("load",currencyConvert);
+
